43d266ac1c966f3d3e251529195e2f3b495215d9
羿均 陳 <kitty.2006.25@gmail.com>
2026-05-04 17:00:21 +0800

    加入專案檔案。

---
+++ frmMediaPlayer.Designer.cs
@@ -0,0 +1,246 @@
+﻿namespace MediaPlayer
+{
+    partial class frmMediaPlayer
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmMediaPlayer));
+            this.palButton = new System.Windows.Forms.Panel();
+            this.btnMute = new System.Windows.Forms.Button();
+            this.lblVolume = new System.Windows.Forms.Label();
+            this.tkbVolume = new System.Windows.Forms.TrackBar();
+            this.lblTime = new System.Windows.Forms.Label();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.btnPause = new System.Windows.Forms.Button();
+            this.btnPlay = new System.Windows.Forms.Button();
+            this.btnBrowser = new System.Windows.Forms.Button();
+            this.wmpVideo = new AxWMPLib.AxWindowsMediaPlayer();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.palButton.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.tkbVolume)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.wmpVideo)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // palButton
+            // 
+            this.palButton.Controls.Add(this.btnMute);
+            this.palButton.Controls.Add(this.lblVolume);
+            this.palButton.Controls.Add(this.tkbVolume);
+            this.palButton.Controls.Add(this.lblTime);
+            this.palButton.Controls.Add(this.trackBar1);
+            this.palButton.Controls.Add(this.btnStop);
+            this.palButton.Controls.Add(this.btnPause);
+            this.palButton.Controls.Add(this.btnPlay);
+            this.palButton.Controls.Add(this.btnBrowser);
+            this.palButton.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.palButton.Location = new System.Drawing.Point(0, 423);
+            this.palButton.Margin = new System.Windows.Forms.Padding(1, 1, 1, 1);
+            this.palButton.Name = "palButton";
+            this.palButton.Size = new System.Drawing.Size(884, 188);
+            this.palButton.TabIndex = 1;
+            // 
+            // btnMute
+            // 
+            this.btnMute.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.btnMute.Font = new System.Drawing.Font("新細明體", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.btnMute.Location = new System.Drawing.Point(329, 66);
+            this.btnMute.Margin = new System.Windows.Forms.Padding(1, 1, 1, 1);
+            this.btnMute.Name = "btnMute";
+            this.btnMute.Size = new System.Drawing.Size(138, 43);
+            this.btnMute.TabIndex = 8;
+            this.btnMute.Text = "🔇";
+            this.btnMute.UseVisualStyleBackColor = true;
+            this.btnMute.Click += new System.EventHandler(this.btnMute_Click);
+            // 
+            // lblVolume
+            // 
+            this.lblVolume.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblVolume.AutoSize = true;
+            this.lblVolume.Font = new System.Drawing.Font("標楷體", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.lblVolume.Location = new System.Drawing.Point(748, 75);
+            this.lblVolume.Margin = new System.Windows.Forms.Padding(1, 0, 1, 0);
+            this.lblVolume.Name = "lblVolume";
+            this.lblVolume.Size = new System.Drawing.Size(86, 16);
+            this.lblVolume.TabIndex = 7;
+            this.lblVolume.Text = "音量: 50%";
+            // 
+            // tkbVolume
+            // 
+            this.tkbVolume.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.tkbVolume.Location = new System.Drawing.Point(515, 65);
+            this.tkbVolume.Margin = new System.Windows.Forms.Padding(1, 1, 1, 1);
+            this.tkbVolume.Maximum = 100;
+            this.tkbVolume.Name = "tkbVolume";
+            this.tkbVolume.Size = new System.Drawing.Size(235, 45);
+            this.tkbVolume.TabIndex = 6;
+            this.tkbVolume.TickFrequency = 10;
+            this.tkbVolume.Value = 50;
+            this.tkbVolume.Scroll += new System.EventHandler(this.tkbVolume_Scroll);
+            // 
+            // lblTime
+            // 
+            this.lblTime.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblTime.Font = new System.Drawing.Font("標楷體", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.lblTime.Location = new System.Drawing.Point(748, 12);
+            this.lblTime.Margin = new System.Windows.Forms.Padding(1, 0, 1, 0);
+            this.lblTime.Name = "lblTime";
+            this.lblTime.Size = new System.Drawing.Size(130, 16);
+            this.lblTime.TabIndex = 5;
+            this.lblTime.Text = "00:00 / 00:00";
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.trackBar1.Location = new System.Drawing.Point(14, 12);
+            this.trackBar1.Margin = new System.Windows.Forms.Padding(1, 1, 1, 1);
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(736, 45);
+            this.trackBar1.TabIndex = 4;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            this.trackBar1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.trackBar1_MouseDown);
+            this.trackBar1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.trackBar1_MouseUp);
+            // 
+            // btnStop
+            // 
+            this.btnStop.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.btnStop.BackColor = System.Drawing.Color.MistyRose;
+            this.btnStop.Enabled = false;
+            this.btnStop.Font = new System.Drawing.Font("標楷體", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.btnStop.Location = new System.Drawing.Point(228, 132);
+            this.btnStop.Margin = new System.Windows.Forms.Padding(1, 1, 1, 1);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(70, 50);
+            this.btnStop.TabIndex = 3;
+            this.btnStop.Text = "停止";
+            this.btnStop.UseVisualStyleBackColor = false;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // btnPause
+            // 
+            this.btnPause.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.btnPause.Enabled = false;
+            this.btnPause.Font = new System.Drawing.Font("標楷體", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.btnPause.Location = new System.Drawing.Point(154, 132);
+            this.btnPause.Margin = new System.Windows.Forms.Padding(1, 1, 1, 1);
+            this.btnPause.Name = "btnPause";
+            this.btnPause.Size = new System.Drawing.Size(72, 50);
+            this.btnPause.TabIndex = 2;
+            this.btnPause.Text = "暫停";
+            this.btnPause.UseVisualStyleBackColor = true;
+            this.btnPause.Click += new System.EventHandler(this.btnPause_Click);
+            // 
+            // btnPlay
+            // 
+            this.btnPlay.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.btnPlay.BackColor = System.Drawing.Color.Honeydew;
+            this.btnPlay.Enabled = false;
+            this.btnPlay.Font = new System.Drawing.Font("標楷體", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.btnPlay.Location = new System.Drawing.Point(78, 132);
+            this.btnPlay.Margin = new System.Windows.Forms.Padding(1, 1, 1, 1);
+            this.btnPlay.Name = "btnPlay";
+            this.btnPlay.Size = new System.Drawing.Size(73, 50);
+            this.btnPlay.TabIndex = 1;
+            this.btnPlay.Text = "播放";
+            this.btnPlay.UseVisualStyleBackColor = false;
+            this.btnPlay.Click += new System.EventHandler(this.btnPlay_Click);
+            // 
+            // btnBrowser
+            // 
+            this.btnBrowser.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.btnBrowser.Font = new System.Drawing.Font("標楷體", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.btnBrowser.Location = new System.Drawing.Point(5, 132);
+            this.btnBrowser.Margin = new System.Windows.Forms.Padding(1, 1, 1, 1);
+            this.btnBrowser.Name = "btnBrowser";
+            this.btnBrowser.Size = new System.Drawing.Size(70, 50);
+            this.btnBrowser.TabIndex = 0;
+            this.btnBrowser.Text = "瀏覽";
+            this.btnBrowser.UseVisualStyleBackColor = true;
+            this.btnBrowser.Click += new System.EventHandler(this.btnBrowser_Click);
+            // 
+            // wmpVideo
+            // 
+            this.wmpVideo.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.wmpVideo.Enabled = true;
+            this.wmpVideo.Location = new System.Drawing.Point(0, 0);
+            this.wmpVideo.Margin = new System.Windows.Forms.Padding(1, 1, 1, 1);
+            this.wmpVideo.Name = "wmpVideo";
+            this.wmpVideo.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("wmpVideo.OcxState")));
+            this.wmpVideo.Size = new System.Drawing.Size(884, 611);
+            this.wmpVideo.TabIndex = 0;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // frmMediaPlayer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(884, 611);
+            this.Controls.Add(this.palButton);
+            this.Controls.Add(this.wmpVideo);
+            this.KeyPreview = true;
+            this.Margin = new System.Windows.Forms.Padding(1, 1, 1, 1);
+            this.Name = "frmMediaPlayer";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "多媒體播放器";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.frmMediaPlayer_FormClosing);
+            this.Shown += new System.EventHandler(this.frmMediaPlayer_Shown);
+            this.DragDrop += new System.Windows.Forms.DragEventHandler(this.frmMediaPlayer_DragDrop);
+            this.DragEnter += new System.Windows.Forms.DragEventHandler(this.frmMediaPlayer_DragEnter);
+            this.palButton.ResumeLayout(false);
+            this.palButton.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.tkbVolume)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.wmpVideo)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private AxWMPLib.AxWindowsMediaPlayer wmpVideo;
+        private System.Windows.Forms.Panel palButton;
+        private System.Windows.Forms.Button btnBrowser;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.Button btnPause;
+        private System.Windows.Forms.Button btnPlay;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.Label lblTime;
+        private System.Windows.Forms.TrackBar tkbVolume;
+        private System.Windows.Forms.Label lblVolume;
+        private System.Windows.Forms.Button btnMute;
+    }
+}
+
---
+++ frmMediaPlayer.cs
@@ -0,0 +1,212 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace MediaPlayer
+{
+    public partial class frmMediaPlayer : Form
+    {
+        private bool isDragging = false;
+
+        public frmMediaPlayer()
+        {
+            InitializeComponent();
+            ResetUI();
+            timer1.Interval = 100;
+            this.AllowDrop = true;
+        }
+
+        private void ResetUI()
+        {
+            btnPlay.Enabled = false;
+            btnPause.Enabled = false;
+            btnStop.Enabled = false;
+            trackBar1.Value = 0;
+            trackBar1.Enabled = false;
+            tkbVolume.Value = 50;
+            wmpVideo.settings.volume = tkbVolume.Value;
+        }
+
+        private void StartPlaying(string filePath)
+        {
+            if (string.IsNullOrEmpty(filePath)) return;
+
+            // 1. 設定影片路徑並播放
+            wmpVideo.URL = filePath;
+            wmpVideo.Ctlcontrols.play();
+
+            // 2. 啟動計時器以更新進度條
+            timer1.Start();
+
+            // 3. 啟用控制按鈕
+            btnPlay.Enabled = true;
+            btnPause.Enabled = true;
+            btnStop.Enabled = true;
+            trackBar1.Enabled = true;
+
+            // 4. 更新視窗標題
+            this.Text = "正在播放: " + System.IO.Path.GetFileName(filePath);
+        }
+
+        // 1. 當檔案拖到視窗上方時
+        private void frmMediaPlayer_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+                e.Effect = DragDropEffects.Copy; // 顯示增加圖示
+        }
+
+        // 2. 當檔案放開時
+        private void frmMediaPlayer_DragDrop(object sender, DragEventArgs e)
+        {
+            string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
+            if (files.Length > 0)
+            {
+                // 使用封裝好的播放邏輯
+                StartPlaying(files[0]);
+            }
+        }
+
+        private void btnBrowser_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = "影片檔案|*.mp4;*.wmv;*.avi;*.mkv|所有檔案|*.*";
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                // 使用封裝好的播放邏輯
+                StartPlaying(ofd.FileName);
+            }
+        }
+
+        private void btnPlay_Click(object sender, EventArgs e)
+        {
+            wmpVideo.Ctlcontrols.play();
+            timer1.Start();
+        }
+
+        private void btnPause_Click(object sender, EventArgs e)
+        {
+            wmpVideo.Ctlcontrols.pause();
+            timer1.Stop();
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            wmpVideo.Ctlcontrols.stop();
+            timer1.Stop();
+            ResetUI();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (wmpVideo.currentMedia != null && wmpVideo.currentMedia.duration > 0)
+            {
+                // 動態設定 Maximum (秒數 * 10 提高精度)
+                trackBar1.Maximum = (int)(wmpVideo.currentMedia.duration * 10);
+
+                // 重點：當使用者「不在拉動」滑塊時，才由計時器自動更新位置
+                if (!isDragging)
+                {
+                    int currentPos = (int)(wmpVideo.Ctlcontrols.currentPosition * 10);
+                    if (currentPos > trackBar1.Maximum) currentPos = trackBar1.Maximum;
+                    trackBar1.Value = currentPos;
+                }
+
+                // 時間文字同步 (直接讀取 WMP 的字串最準)
+                string curTime = wmpVideo.Ctlcontrols.currentPositionString;
+                string totalTime = wmpVideo.currentMedia.durationString;
+
+                if (!string.IsNullOrEmpty(curTime) && !string.IsNullOrEmpty(totalTime))
+                {
+                    lblTime.Text = $"{curTime} / {totalTime}";
+                }
+
+                // 播放結束後的處理
+                if (wmpVideo.playState == WMPLib.WMPPlayState.wmppsStopped)
+                {
+                    timer1.Stop();
+                    ResetUI();
+                }
+            }
+        }
+
+        private void trackBar1_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (wmpVideo.currentMedia == null || wmpVideo.currentMedia.duration == 0)
+            {
+                isDragging = false;
+                return;
+            }
+            isDragging = true;
+        }
+
+        private void trackBar1_MouseUp(object sender, MouseEventArgs e)
+        {
+            if (wmpVideo.currentMedia != null)
+            {
+                // 核心修正：只在放開滑鼠時執行一次跳轉，避免解碼器卡死
+                wmpVideo.Ctlcontrols.currentPosition = (double)trackBar1.Value / 10.0;
+            }
+            isDragging = false;
+        }
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            // 這裡不要去動 wmpVideo.Ctlcontrols.currentPosition！
+            // 只需要更新畫面上的時間文字，讓使用者知道現在拉到哪了
+            if (wmpVideo.currentMedia != null)
+            {
+                double tempPos = (double)trackBar1.Value / 10.0;
+                // 我們手動算出格式化時間，避免頻繁調用 WMP 組件
+                TimeSpan ts = TimeSpan.FromSeconds(tempPos);
+                lblTime.Text = $"{ts.Minutes:D2}:{ts.Seconds:D2} / {wmpVideo.currentMedia.durationString}";
+            }
+        }
+
+        private void frmMediaPlayer_Shown(object sender, EventArgs e)
+        {
+            this.CenterToScreen();
+        }
+
+        private void frmMediaPlayer_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            DialogResult result = MessageBox.Show("確定要結束播放並關閉程式嗎？", "確認關閉",
+                                    MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+
+            if (result == DialogResult.No)
+            {
+                e.Cancel = true; // 取消關閉動作
+            }
+        }
+
+        private void tkbVolume_Scroll(object sender, EventArgs e)
+        {
+            wmpVideo.settings.volume = tkbVolume.Value;
+            lblVolume.Text = $"音量: {tkbVolume.Value}%";
+        }
+
+        private void btnMute_Click(object sender, EventArgs e)
+        {
+            wmpVideo.settings.mute = !wmpVideo.settings.mute;
+
+            // 根據狀態改變按鈕文字，讓使用者知道現在的情況
+            if (wmpVideo.settings.mute)
+            {
+                btnMute.Text = "🔊";
+                tkbVolume.Enabled = false;
+            }
+            else
+            {
+                btnMute.Text = "🔇";
+                tkbVolume.Enabled = true;
+            }
+        }
+    }
+}
